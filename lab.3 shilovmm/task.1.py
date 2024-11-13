81b6aac63c4abf69431018e5f39c66c314c59b7b
maksul'ka <148970035+chayniki@users.noreply.github.com>
2024-11-14 00:18:37 +0300

    Add files via upload

---
+++ lab.3 shilovmm/task.1.py
@@ -0,0 +1,17 @@
+# TODO Напишите функцию для поиска индекса товара
+
+def search_index(items, required_item):
+
+  if required_item in items:
+   return items.index(required_item)
+  else:
+   return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
---
+++ lab.3 shilovmm/task.2.py
@@ -0,0 +1,12 @@
+# TODO Напишите функцию find_common_participants
+def find_common_participants(first, second, divider = ','):
+ common_members = list(set(first.split(divider)).intersection(second.split(divider)))
+
+ common_members.sort()
+ return common_members
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group, participants_second_group, '|'))
+# TODO Провеьте работу функции с разделителем отличным от запятой
